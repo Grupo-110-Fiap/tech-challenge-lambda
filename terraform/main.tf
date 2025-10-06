a5544a0945b1bd974a928cdc6c6fb3ccef6bfcb6
Yumi <laryyumi@gmail.com>
2025-10-06 00:35:58 -0300

    removed aws from required_providers

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
     github = {
       source  = "integrations/github"
       version = "6.4.0"
